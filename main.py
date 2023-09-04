a9dc53c0e41283d83e7fc50295aa3bda8001d4a1
abykov <abykov@uma.tech>
2023-09-04 13:53:27 +0300

    Домашняя работа 1

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 
-# Для начала определим настройки запуска
-hostName = "localhost" # Адрес для доступа по сети
-serverPort = 8081 # Порт для доступа по сети
+hostName = "localhost"
+serverPort = 8081
+
 
 class MyServer(BaseHTTPRequestHandler):
-    """
-        Специальный класс, который отвечает за
-        обработку входящих запросов от клиентов
-    """
+
     def __get_html_content(self):
-        """Метод получения html разметки из файла index.html"""
         file_path = "index.html"
 
         try:
@@ -25,31 +21,24 @@ class MyServer(BaseHTTPRequestHandler):
             print(f"Ошибка при чтении файла: {e}")
             return None
 
-
     def do_GET(self):
-        """ Метод для обработки входящих GET-запросов """
         query_components = parse_qs(urlparse(self.path).query)
         print(query_components)
         page_content = self.__get_html_content()
-        self.send_response(200) # Отправка кода ответа
-        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
-        self.end_headers() # Завершение формирования заголовков ответа
-        self.wfile.write(bytes(page_content, "utf-8")) # Тело ответа
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.end_headers()  # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, "utf-8"))  # Тело ответа
+
 
 if __name__ == "__main__":
-    # Инициализация веб-сервера, который будет по заданным параметрам в сети
-    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
-        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
         webServer.serve_forever()
     except KeyboardInterrupt:
-        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
         pass
 
-    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
     webServer.server_close()
     print("Server stopped.")
-
